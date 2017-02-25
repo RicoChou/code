1a0d218dcbce10d6950bc2f01db9205ff05d0374
Rico Chow <ecust_rico@163.com>
2017-02-25 21:45:39 +0800

    434

---
+++ leetcode/434_Number of Segments in a String.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int countSegments(String s) {
+        return ("x " + s).split(" +").length - 1;
+    }
+}
+
+public class Solution {
+    public int countSegments(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++)
+            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' '))
+                count++;
+        return count;
+    }
+}
