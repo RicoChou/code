cc8da597ba2438c20e8b9dc1d4d6d54d2f02fc6b
Rico Chow <ecust_rico@163.com>
2017-03-03 17:56:32 +0800

    java graph algorithms

--- java/graph/Vertex.java
+++ java/graph/Vertex.java
@@ -13,14 +13,20 @@ public class Vertex<T> {
     public List<Edge> adjacentList;
     /* 是否已访问过 */
     public boolean visited;
+    /* 是否已知路径为最短 */
+    public boolean known;
     /* 入度 */
     public int indegree;
+    /* 单源路径长 */
+    public int dist;
 
     public Vertex(T label) {
         this.label = label;
         this.visited = false;
-        adjacentList = new LinkedList<>();
-        indegree = 0;
+        this.adjacentList = new LinkedList<>();
+        this.indegree = 0;
+        this.dist = Integer.MAX_VALUE;
+        this.known = false;
     }
 
     @Override
