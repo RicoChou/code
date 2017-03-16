d1e4b5f2ce7c6894c775c2b97c5d636459c2b615
Rico Chow <ecust_rico@163.com>
2017-03-16 22:21:37 +0800

    206

---
+++ leetcode/206_Reverse Linked List.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+// recursive
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
+
+// iterative
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
