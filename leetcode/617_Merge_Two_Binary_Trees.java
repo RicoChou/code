d95193f822eda03868cd6cd6501c0fdeb9dfb489
Rico Chow <ecust_rico@163.com>
2018-04-03 18:17:08 +0800

    rename files: replace blank with _

--- leetcode/617_Merge_Two_Binary_Trees.java
+++ leetcode/617_Merge_Two_Binary_Trees.java
@@ -1,10 +1,10 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
 class Solution {
@@ -20,11 +20,12 @@ class Solution {
         t1.right = mergeTrees(t1.right, t2.right);
         return t1;
     }
+
     public TreeNode mergeTrees2(TreeNode t1, TreeNode t2) {
         if (t1 == null)
             return t2;
-        Stack < TreeNode[] > stack = new Stack < > ();
-        stack.push(new TreeNode[] {t1, t2});
+        Stack<TreeNode[]> stack = new Stack<>();
+        stack.push(new TreeNode[]{t1, t2});
         while (!stack.isEmpty()) {
             TreeNode[] t = stack.pop();
             if (t[0] == null || t[1] == null) {
@@ -34,12 +35,12 @@ class Solution {
             if (t[0].left == null) {
                 t[0].left = t[1].left;
             } else {
-                stack.push(new TreeNode[] {t[0].left, t[1].left});
+                stack.push(new TreeNode[]{t[0].left, t[1].left});
             }
             if (t[0].right == null) {
                 t[0].right = t[1].right;
             } else {
-                stack.push(new TreeNode[] {t[0].right, t[1].right});
+                stack.push(new TreeNode[]{t[0].right, t[1].right});
             }
         }
         return t1;
